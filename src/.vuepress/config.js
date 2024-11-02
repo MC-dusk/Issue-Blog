bdd43c4359b56570831dcf370dbda3eb51b9659b
dusk <52311860+MC-dusk@users.noreply.github.com>
2024-11-02 17:00:24 +0800

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -41,7 +41,7 @@ module.exports = {
     [
       "@vssue/vuepress-plugin-vssue",
       {
-        platform: "github",
+        platform: "github-v3",
         ...customConfig.vssueConfig
       }
     ]
